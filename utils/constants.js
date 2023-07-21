0e0940cd3be4295653ecd90286d9c7943938c22a
Кознова Тома <119004561+Servicebox@users.noreply.github.com>
2023-07-21 11:05:33 +0300

    исправления в package,user

--- utils/constants.js
+++ utils/constants.js
@@ -1,26 +1,16 @@
-// генерация ключа в терминале через команду
+const JWT_TOKEN_EXPIRES = '7d';
+
+// генерация сикрет-ключа в терминале через
 // node -e "console.log(require('crypto').randomBytes(32).toString('hex'));"
-
 const SECRET_KEY = 'e48c3013265b869e0e40ebd19adf5f1f12b90129191b01ef7f7bbd722937a074';
-
-// лимит
-const rateLimiter = require('express-rate-limit');
-
-const limiter = rateLimiter({
-  max: 120,
-  windowMS: 45000,
-  message: 'В настоящий момент превышено количество запросов на сервер. Пожалуйста, попробуйте повторить позже',
-});
-
 // регулярное выражение
-const URL_REGEX = /^(https?:\/\/)?([\w-]{1,32}\.[\w-]{1,32})[^\s!"#$%&'()*+,:;<=>?@[\\\]`{|}~]*$/;
+const URL_REGEX = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/;
 const CREATED_STATUS = 201;
 
 module.exports = {
   SECRET_KEY,
   URL_REGEX,
   CREATED_STATUS,
-  rateLimiter,
-  limiter,
+  JWT_TOKEN_EXPIRES,
 
 };
